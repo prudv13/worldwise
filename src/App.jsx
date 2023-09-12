7f7484833688273fdf71df976c9c2135ccb9b9a8
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-12 22:29:48 +0530

    implemented main pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Home from './pages/Home';
+import PageNotFound from './pages/PageNotFound';
 
 const App = () => {
   return (
     <div>
-      <h1>Worldwise</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
     </div>
   )
 }

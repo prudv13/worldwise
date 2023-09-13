bf341988c5ad1390bd2a9fa0290fec013a099099
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-13 18:02:13 +0530

    programmatic navigation with useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Homepage from './pages/Homepage';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
+import Form from './components/Form';
 
 
 const CITIES_URL = 'https://apidata-zkgz.onrender.com/cities';
@@ -51,7 +52,7 @@ const App = () => {
           <Route path='cities/:id' element={<City />} />
           
           <Route path='countries' element={<CountryList cities={cities} isLoading={isLoading} />} />
-          <Route path='form' element={<p>Form</p>} />
+          <Route path='form' element={<Form />} />
         </Route>
 
         <Route path='*' element={<PageNotFound />} />

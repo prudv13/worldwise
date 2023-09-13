174b41760105ced357c8062c04b99cd51b5ae0c8
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-13 09:10:44 +0530

    added assets, css modules

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Home from './pages/Home';
 import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
+        <Route path='app' element={<AppLayout />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import PageNav from '../components/PageNav';
+import { Link } from 'react-router-dom';
+import AppLayout from './AppLayout';
 
 const Home = () => {
   return (
     <div>
         <PageNav />
         <h1>Worldwise</h1>
+        <AppLayout />
+
+        <Link to='/app'>Go to the app</Link>
     </div>
   )
 }
